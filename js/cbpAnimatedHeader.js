9bb6101bb162599e646feeb4254f1459c9e211cd
jjjhammer <hammer.julian@gmail.com>
2015-04-10 10:59:08 +0200

    mobile optimization, taking out testimonials

--- js/cbpAnimatedHeader.js
+++ js/cbpAnimatedHeader.js
@@ -13,7 +13,7 @@ var cbpAnimatedHeader = (function() {
 	var docElem = document.documentElement,
 		header = document.querySelector( '.navbar-default' ),
 		didScroll = false,
-		changeHeaderOn = 150;
+		changeHeaderOn = 70;
 
 	function init() {
 		window.addEventListener( 'scroll', function( event ) {
